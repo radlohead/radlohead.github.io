8a3044e7e5792dd490f12540b830ad2a7d2c18ae
minho park <radlohead@naver.com>
2017-09-06 14:04:57 +0900

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -2,53 +2,50 @@ $.ajax({
     type:"GET",
     url:"data.xml",
     success:function(xml){
+        const doc = document;
         const header = xml.querySelector('header');
         const caption = header.querySelector('description').innerHTML;
         const columns = header.querySelector('columns').innerHTML;
         const body = xml.querySelector('body');
         const items = body.querySelectorAll('item');
-        let column = columns.split(',');
-        let indsLclsCd = [];
-        let indsLclsNm = [];
-        let stdrDt = [];
-
-        function func(arr, str) {
-            for(let i=0; i < items.length; i++) {
-                arr.push(items[i].querySelector(str).innerHTML);
-            }
-        }
-        func(indsLclsCd, 'indsLclsCd');
-        func(indsLclsNm, 'indsLclsNm');
-        func(stdrDt, 'stdrDt');
-
-        const doc = document;
         const root = doc.querySelector('#root');
         const table = doc.createElement('table');
         const title = doc.createElement('caption');
+        const tr = doc.createElement('tr');
+        let column = columns.split(',');
+        let totalColumn = [];
 
         title.innerHTML = caption;
         table.appendChild(title);
+        function getText(target, i) {
+            return target.map(v => {
+                return items[i].querySelector(v).innerHTML
+            });
+        }
+        function func(tagNames) {
+            for(let i=0; i < items.length; i++) {
+                totalColumn.push(getText(tagNames, i));
+            }
+        }
+        func(['indsLclsCd', 'indsLclsNm', 'stdrDt']);
 
-        const tr = doc.createElement('tr');
-        for(let i=0; i < 3; i++) {
+        for(let i=0; i < column.length; i++) {
             const th = doc.createElement('th');
             th.innerHTML = column[i];
             tr.appendChild(th);
         }
         table.appendChild(tr);
-        for(let i=0; i < items.length; i++) {
+
+        totalColumn.reduce((table,  data) => {
             const tr = doc.createElement('tr');
-            const td = doc.createElement('td');
-            const td2 = doc.createElement('td');
-            const td3 = doc.createElement('td');
-            td.innerHTML = indsLclsCd[i];
-            td2.innerHTML = indsLclsNm[i];
-            td3.innerHTML = stdrDt[i];
-            tr.appendChild(td);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
+            for(let i=0; i<3; i++) {
+                const td = doc.createElement('td');
+                td.innerHTML = data[i];
+                tr.appendChild(td);
+            }
             table.appendChild(tr);
-        }
+            return table;
+        }, table);
 
         root.appendChild(table);
     }
