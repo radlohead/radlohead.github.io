aa4b9a83eaa1a551892df1f908d12557da71998e
minho park <radlohead@naver.com>
2017-09-06 10:26:30 +0900

    Add files via upload

---
+++ assets/app2.js
@@ -0,0 +1,65 @@
+$.ajax({
+    type:"GET",
+    url:"data2.xml",
+    success:function(xml){
+        const header = xml.querySelector('header');
+        const caption = header.querySelector('description').innerHTML;
+        const columns = header.querySelector('columns').innerHTML;
+        const body = xml.querySelector('body');
+        const items = body.querySelectorAll('item');
+        let column = columns.split(',');
+        let indsLclsCd = [];
+        let indsLclsNm = [];
+        let indsMclsCd = [];
+        let indsMclsNm = [];
+        let stdrDt = [];
+
+        function func(arr, str) {
+            for(let i=0; i < items.length; i++) {
+                arr.push(items[i].querySelector(str).innerHTML);
+            }
+        }
+        func(indsLclsCd, 'indsLclsCd');
+        func(indsLclsNm, 'indsLclsNm');
+        func(indsMclsCd, 'indsMclsCd');
+        func(indsMclsNm, 'indsMclsNm');
+        func(stdrDt, 'stdrDt');
+
+        const doc = document;
+        const root = doc.querySelector('#root');
+        const table = doc.createElement('table');
+        const title = doc.createElement('caption');
+
+        title.innerHTML = caption;
+        table.appendChild(title);
+
+        const tr = doc.createElement('tr');
+        for(let i=0; i < 5; i++) {
+            const th = doc.createElement('th');
+            th.innerHTML = column[i];
+            tr.appendChild(th);
+        }
+        table.appendChild(tr);
+        for(let i=0; i < items.length; i++) {
+            const tr = doc.createElement('tr');
+            const td = doc.createElement('td');
+            const td2 = doc.createElement('td');
+            const td3 = doc.createElement('td');
+            const td4 = doc.createElement('td');
+            const td5 = doc.createElement('td');
+            td.innerHTML = indsLclsCd[i];
+            td2.innerHTML = indsLclsNm[i];
+            td3.innerHTML = indsMclsCd[i];
+            td4.innerHTML = indsMclsNm[i];
+            td5.innerHTML = stdrDt[i];
+            tr.appendChild(td);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            table.appendChild(tr);
+        }
+
+        root.appendChild(table);
+    }
+});
